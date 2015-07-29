eb59503328f1e10ae5525611ff0622c7b8791934
Hackbright Student <info@hackbrightacademy.com>
2015-07-28 17:57:44 -0700

    We seeded.  Now working on removing white space after each line (rstsrip).

--- seed.py
+++ seed.py
@@ -3,9 +3,18 @@
 from model import User, Rating, Movie, connect_to_db, db
 from server import app
 
-
 def load_users():
     """Load users from u.user into database."""
+    open_file = open("seed_data/u.user")
+    for line in open_file:
+        listified_user_row = line.split("|").rstrip("\n")
+        # print   listified_user_row
+        user_id = listified_user_row[0]
+        age = listified_user_row[1]
+        zipcode =listified_user_row[4]
+        new_user = User(user_id = user_id, age = age, zipcode = zipcode)
+        db.session.add(new_user)
+    db.session.commit()
 
 
 def load_movies():
@@ -20,5 +29,5 @@ if __name__ == "__main__":
     connect_to_db(app)
 
     load_users()
-    load_movies()
-    load_ratings()
+    # load_movies()
+    # load_ratings()
